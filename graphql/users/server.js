539edffef47114808416ba9c65a3069f52dbabc9
Atanu Paul <atanu.paul.dev@gmail.com>
2022-10-06 01:39:42 +0530

    adding first project

---
+++ graphql/users/server.js
@@ -0,0 +1,23 @@
+"use strict";
+require("dotenv").config();
+require("colors");
+
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+
+const server = express();
+
+const PORT = process.env.APP_PORT;
+
+server.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+  })
+);
+
+server.listen(PORT, () => {
+  console.log(
+    `EXPRESS GRAPHQL SERVER LISTING ON PORT NUMBER : ${PORT}`.underline.red
+  );
+});
